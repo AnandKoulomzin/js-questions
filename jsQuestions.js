1801d84b4ff8d4adac01e31ac84e35b107823a0a
anandkoulomzin <anand_koulomzin25@milton.edu>
2024-03-05 12:15:18 -0500

    finished js questions

--- jsQuestions.js
+++ jsQuestions.js
@@ -31,55 +31,116 @@
 // words.push(console);
 // words.push(x);
 
-const numbers = [Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29)];
-const distances = [];
-for (let i = 0; i < numbers.length - 1; i++) {
-    var distance = numbers[i]-numbers[i+1]
-    if (distance<0) {
-        distance = distance*(-1);
+// const numbers = [Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29), Math.floor(Math.random()*58+29)];
+// const distances = [];
+// for (let i = 0; i < numbers.length - 1; i++) {
+//     var distance = numbers[i]-numbers[i+1]
+//     if (distance<0) {
+//         distance = distance*(-1);
+//     }
+//     distances.push(distance);
+//     // console.log(numbers[i]);
+// }
+// var greatestDistance = 0;
+// var distancesIndex;
+// // console.log("****");
+// for (let x = 0; x < distances.length; x++) {
+//     // console.log(distances[x]);
+//     if (distances[x] > greatestDistance) {
+//         greatestDistance = distances[x];
+//         distancesIndex = x;
+//     }
+// }
+// console.log("greatest distance= " + greatestDistance + " between elements " + (distancesIndex+1) + " and " + (distancesIndex+2));
+
+// const numbersSquared = numbers.map(square)
+// function square(num) {
+//   return num * num;
+// }
+// console.log("original numbers: " + numbersSquared);
+
+// var largestNumberSquared = 0;
+// for (let i = 0; i < numbersSquared.length; i++) {
+//     if (numbersSquared[i] > largestNumberSquared) {
+//         largestNumberSquared = numbersSquared[i];
+//     }
+// }
+// console.log("largest number squared: " + largestNumberSquared);
+// const squaredResult = numbersSquared.filter(largerThanHalf);
+// function largerThanHalf(num){
+//     return num > (largestNumberSquared/2);
+// }
+// console.log("filtered numbers squared array: " + squaredResult);
+
+// squaredResult.reduce(getSum, 0);
+// function getSum(total, num) {
+//   return total + Math.round(num);
+// }
+// console.log("filtered numbers squared array sum= " + squaredResult.reduce(getSum, 0))
+
+// squaredResult.forEach(print);
+// function print(num) {
+//     console.log(num);
+// }
+
+class Movie {
+    constructor(releaseDate, title, director) {
+        this.releaseDate = releaseDate;
+        this.title = title;
+        this.director = director;
     }
-    distances.push(distance);
-    // console.log(numbers[i]);
-}
-var greatestDistance = 0;
-var distancesIndex;
-// console.log("****");
-for (let x = 0; x < distances.length; x++) {
-    // console.log(distances[x]);
-    if (distances[x] > greatestDistance) {
-        greatestDistance = distances[x];
-        distancesIndex = x;
+    print(){
+        console.log("length: " + this.releaseDate + ". title: " + this.title + ". director: " + this.director);
     }
 }
-console.log("greatest distance= " + greatestDistance + " between elements " + (distancesIndex+1) + " and " + (distancesIndex+2));
-
-const numbersSquared = numbers.map(square)
-function square(num) {
-  return num * num;
+const movies = [];
+const thePhantomMenace = new Movie(1999, "The Phantom Menace", "George Lucas");
+movies.push(thePhantomMenace);
+const attackOfTheClones = new Movie(2002, "Attack Of The Clones", "George Lucas");
+movies.push(attackOfTheClones);
+const revengeOfTheSith = new Movie(2005, "Revenge Of The Sith", "George Lucas");
+movies.push(revengeOfTheSith);
+const aNewHope = new Movie(1977, "A New Hope", "George Lucas");
+movies.push(aNewHope);
+const theEmpireStrikesBack = new Movie(1980, "The Empire Strikes Back", "George Lucas");
+movies.push(theEmpireStrikesBack);
+const returnOfTheJedi = new Movie(1983, "Return Of The Jedi", "George Lucas");
+movies.push(returnOfTheJedi);
+const theAvengers = new Movie(2012, "The Avengers", "Joss Whedon");
+movies.push(theAvengers);
+const ageOfUltron = new Movie(2015, "Age Of Ultron", "Joss Whedon");
+movies.push(ageOfUltron);
+const infinityWar = new Movie(2018, "Infinity War", "Anthony Russo");
+movies.push(infinityWar);
+const endgame = new Movie(2019, "Endgame", "Anthony Russo");
+movies.push(endgame);
+aNewHope.print();
+//how to make a class full of objects javascript w3 schools
+for (let i = 0; i < movies.length; i++) {
+    console.log(movies[i].title);
 }
-console.log("original numbers: " + numbersSquared);
+const moviesFiltered = movies.filter(e => 
+    e.title == aNewHope.title
+    )
+    
+console.log("movie i found: " );
+moviesFiltered[0].print();
 
-var largestNumberSquared = 0;
-for (let i = 0; i < numbersSquared.length; i++) {
-    if (numbersSquared[i] > largestNumberSquared) {
-        largestNumberSquared = numbersSquared[i];
-    }
+const movieDates = [];
+for (let i = 0; i < movies.length; i++) {
+    movieDates.push(movies[i].releaseDate);
 }
-console.log("largest number squared: " + largestNumberSquared);
-const squaredResult = numbersSquared.filter(largerThanHalf);
-function largerThanHalf(num){
-    return num > (largestNumberSquared/2);
+const moviesMapped = movieDates.map(timeSince1900);
+function timeSince1900(num) {
+    return (num - 1900);
 }
-console.log("filtered numbers squared array: " + squaredResult);
-
-squaredResult.reduce(getSum, 0);
-function getSum(total, num) {
-  return total + Math.round(num);
+console.log("mapped movies: " + moviesMapped);
+movies.forEach(e=>changeMovies(e));
+function changeMovies( movie) {
+   movie.releaseDate = movie.releaseDate-1900;
 }
-console.log("filtered numbers squared array sum= " + squaredResult.reduce(getSum, 0))
-
-squaredResult.forEach(print);
-function print(num) {
-    console.log(num);
+for (let i = 0; i < movies.length; i++) {
+    console.log(movies[i].releaseDate);
 }
-
+movieDates.sort(function(a, b){return b-a});
+console.log(movieDates);
